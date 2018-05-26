7aefcf259e1922fa965441017ac51f62655e16d5
MrK <mrk@MrKs.local>
2018-05-27 01:19:51 +0700

    tuan

---
+++ assets/Scripts/Light.js
@@ -0,0 +1,50 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        Background: cc.Sprite,
+        NextLight: cc.SpriteFrame
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        var seft = this;
+        this.node.on('touchend', (e)=>{
+            var bg = seft.Background.getComponent(cc.Sprite);
+            bg.SpriteFrame = seft.NextLight;
+            console.log(1);
+        });
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
