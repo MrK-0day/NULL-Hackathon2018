890d1a0c38c7630aca25d03442d75770dc941561
MrK <mrk@MrKs.local>
2018-05-27 05:32:12 +0700

    tuan

--- assets/Scripts/game.js
+++ assets/Scripts/game.js
@@ -15,21 +15,50 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad: function () {
-        this.connection = window.io('http://10.45.78.95:3000');
+        console.log('ahihi');
+        this.connection = window.io('http://172.20.10.2:3000/');
         this.side = null;
+        this.padding = 20;
+        var self = this;
         //this.connection.emit('location', 'concac');
-        this.connection.on('left_side', () => {
+        this.connection.on('you_are_player1', () => {
             var player_1 = cc.instantiate(this.player_1);
             this.node.addChild(player_1);
-            this.side = 1;
+            var width = cc.director.getWinSize().width / 2;
+            var height = cc.director.getWinSize().height / 2;
+            player_1.setPosition(-width + this.padding, -height + this.padding);
+
+            var coord = this.node.getChildByName("player_1").getPosition();
+            this.connection.emit('coord_player1', { coord: coord });
+            this.connection.on('render_player2', data => {
+                var player_2 = cc.instantiate(this.player_2);
+                self.node.addChild(player_2);
+                player_2.setPosition(data.x, data.y);
+            });
         });
 
-        this.connection.on('right_side', () => {
+        this.connection.on('you_are_player2', () => {
             var player_2 = cc.instantiate(this.player_2);
             this.node.addChild(player_2);
-            this.side = 2;
-            var player_1 = cc.instantiate(this.player_1);
-            this.node.addChild(player_1);
+            var width = cc.director.getWinSize().width / 2;
+            var height = cc.director.getWinSize().height / 2;
+            player_2.setPosition(width - this.padding, height - this.padding);
+
+            var coord = this.node.getChildByName("player_2").getPosition();
+            this.connection.emit('coord_player2', { coord: coord });
+            this.connection.on('render_player1', data => {
+                var player_1 = cc.instantiate(this.player_1);
+                self.node.addChild(player_1);
+                player_1.setPosition(data.x, data.y);
+            });
+        });
+
+        this.connection.on('change_coord_player1', data => {
+            this.node.getChildByName('player_1').setPosition(data.x, data.y);
+        });
+
+        this.connection.on('change_coord_player2', data => {
+            this.node.getChildByName('player_2').setPosition(data.x, data.y);
         });
     },
 
--- assets/Scripts/player_1.js
+++ assets/Scripts/player_1.js
@@ -9,8 +9,8 @@ cc.Class({
     onLoad () {
         this.keyUp = false;
         this.keyDown = false;
-        this.keyLeft = false;
-        this.keyRight = false;
+        //this.keyLeft = false;
+        //this.keyRight = false;
         this.deltaY = 5;
         this.setInputControl();
     },
@@ -25,15 +25,15 @@ cc.Class({
                 case cc.KEY.down:
                     self.keyDown = true;
                     break;
-                case cc.KEY.left:
-                    self.keyLeft = true;
-                    break;
-                case cc.KEY.right:
-                    self.keyRight = true;
-                    break;
-                case cc.KEY.space:
-                    self.fire = true;
-                    break;
+                // case cc.KEY.left:
+                //     self.keyLeft = true;
+                //     break;
+                // case cc.KEY.right:
+                //     self.keyRight = true;
+                //     break;
+                // case cc.KEY.space:
+                //     self.fire = true;
+                //     break;
             }
         });
 
@@ -45,12 +45,12 @@ cc.Class({
                 case cc.KEY.down:
                     self.keyDown = false;
                     break;
-                case cc.KEY.left:
-                    self.keyLeft = false;
-                    break;
-                case cc.KEY.right:
-                    self.keyRight = false;
-                    break;
+                // case cc.KEY.left:
+                //     self.keyLeft = false;
+                //     break;
+                // case cc.KEY.right:
+                //     self.keyRight = false;
+                //     break;
             }
         });        
     },
@@ -67,15 +67,15 @@ cc.Class({
             if (this.node.y >= -320 + this.deltaY)
                 this.node.y -= this.deltaY;
         } 
-        if (this.keyLeft) {
-            if (this.node.x >= -385 - 95 + this.deltaY)
-                this.node.x -= this.deltaY;
-        }
-        if (this.keyRight) {
-            //stop at middle of screen
-            if (this.node.x <= -95 - this.deltaY)
-            //if (this.node.x <= 385 - this.deltaY)
-                this.node.x += this.deltaY;
-        }
+        // if (this.keyLeft) {
+        //     if (this.node.x >= -385 - 95 + this.deltaY)
+        //         this.node.x -= this.deltaY;
+        // }
+        // if (this.keyRight) {
+        //     //stop at middle of screen
+        //     if (this.node.x <= -95 - this.deltaY)
+        //     //if (this.node.x <= 385 - this.deltaY)
+        //         this.node.x += this.deltaY;
+        // }
     },
 });
